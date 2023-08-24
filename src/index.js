b9f682222b8461434875c92d6aa87b958501f06a
unknown <constantin615profit@gmail.com>
2023-08-24 05:19:56 +0300

    feat: comma

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'font-awesome/css/font-awesome.min.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import 'font-awesome/css/font-awesome.min.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { Provider } from 'react-redux'
 import { store } from './store/store'
 
 ReactDOM.render(
@@ -13,7 +13,7 @@ ReactDOM.render(
             <App />
         </Provider>
     </React.StrictMode>
-    , document.getElementById('root'));
+    , document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
