cd383cd56248fbee93a874fef0667a9eba7bfd57
unknown <constantin615profit@gmail.com>
2023-08-24 05:40:24 +0300

    feat: server config

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,5 +1,5 @@
 export const serverConfig = {
-    BACKEND_URL: 'http://10.10.18.64:4000',
-    API_URL: 'http://10.10.18.64:4000/api',
-    PUBLIC_URL: 'http://10.10.18.64:4000/public'
+    BACKEND_URL: 'http://localhost:4000',
+    API_URL: 'http://localhost:4000/api',
+    PUBLIC_URL: 'http://localhost:4000/public'
 }
